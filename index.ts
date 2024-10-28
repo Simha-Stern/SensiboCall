581b4e14991c49212503d1fa1184dc8595e495cd
SimhaStern <A0533187258@gmail.com>
2024-10-28 09:20:49 +0200

    note

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import yemotRouter from "./src/router/router";
+// import controller from "./src/remoteControl/controller";
 
 const PORT = 3333;
 
 const app = express();
 
 app.use("/", yemotRouter.asExpressRouter);
+// app.use("/", async (req, res) =>
+//   res.send(await controller.getAllDevices("meFwVzgdw4o5TYUuGewFsuAAIUX9sm"))
+// )
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
